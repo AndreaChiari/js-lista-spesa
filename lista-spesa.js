f4b68cc94d70f3e926c70d3e4f41a1c5fd95229d
AndreaChiari <chiariandrea94@gmail.com>
2022-12-14 15:06:50 +0100

    update

---
+++ lista-spesa.js
@@ -0,0 +1,21 @@
+console.log('JS OK')
+
+/*
+1)  creo una lista della spesa
+2)  con un ciclo while la determino
+3)  stampo in pagina 
+*/
+
+const targetelement = document.getElementById('target');
+const list = ['Mandragola', 'belladonna', 'sali'];
+
+
+let i=0;
+text= ""
+while (list[i]){
+    text += list[i];
+    i++;
+
+    console.log(list[i])
+}
+
